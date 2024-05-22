34963b6bb51f4f8d3848db8594810712e138c7e2
silky <noonsilk+-noonsilk@gmail.com>
2024-05-22 10:18:24 +0100

    counterexamples

--- src/Main.hs
+++ src/Main.hs
@@ -4,26 +4,48 @@ module Main where
 import Prelude hiding (init)
 import "hspec" Test.Hspec
 import "QuickCheck" Test.QuickCheck
+import "base" Data.Function ((&))
 
+
+data SomeState =
+  SomeState
+    { ints :: [Int]
+    }
+    deriving (Eq, Show)
+
+emptyState :: SomeState
+emptyState = SomeState []
+
+-- data State'
+--   = State'
+--     { state    :: State
+--     , contents :: SomeState
+--     }
 
 data State
-  = Open
-  | Closed
-  | Final
+  = Open   SomeState
+  | Closed SomeState
+  | Final  SomeState
   deriving (Show, Eq)
 
+instance Arbitrary SomeState where
+  arbitrary = SomeState <$> arbitrary
 
 instance Arbitrary State where
-  arbitrary = elements [ Open, Closed, Final ]
+  arbitrary = oneof
+              [ Open   <$> arbitrary
+              , Closed <$> arbitrary
+              , Final  <$> arbitrary
+              ]
 
 
 {-
    TODO:
 
-- [ ] Model our state!
-- [ ] Think about transitions
+- [x] Model our state!
+- [x] Think about transitions
 - [ ] Explore design choices
-- [ ] Make sure we can represent what we want! Step through states?
+- [x] Make sure we can represent what we want! Step through states?
 - [ ] Maybe some tests
     - [x] Can't get to invalid states?
     - [ ] Can get to some expected states?
@@ -32,63 +54,47 @@ Things to think about:
   - [ ] Save increments and decrements
 -}
 
-init :: State
+init :: SomeState -> State
 init = Open
 
 close :: State -> Maybe State
 close = \case
-  Open   -> Just Closed
-  Closed -> Just Closed
-  _      -> Nothing -- Here
+  Open a   -> Just (Closed a)
+  _        -> Nothing
 
 finalise :: State -> Maybe State
 finalise = \case
-  Closed -> Just Final
-  Final  -> Just Final
-  _      -> Nothing
-
+  Closed a -> Just (Final a)
+  -- Final a  -> Just (Final a)
+  _        -> Nothing
 
 {-
 Properties!
   - [ ] Make sure all transitions move right
   - [ ] If we do enough (non-inc/dec) transitions we get to final; i.e. it terminates.
-  - [ ] Can always close and finalise any state.
+  - [x] Can always close and finalise any state.
 
   Stretch goal:
   - [ ] Finite # of decrements (given no increments being applied)
 -}
 
 
+isFinal :: Maybe State -> Property
+isFinal (Just (Final _)) = property True
+isFinal ms               = counterexample ("  ms expected final but got=" ++ show ms) $ property False
+
 prop_canAlwaysCloseAndFinaliseAnyState :: State -> Property
 prop_canAlwaysCloseAndFinaliseAnyState state =
-  (close state >>= finalise) === Just Final .||. finalise state === Just Final
-
-{-
-
-   Hmm.
-    State -> Bool
-
-  Ways to close a state
-    -> Open -> Closed
-    -> Closed -> Itself -- Illegal
-    -> Final -> ?? -- Illegal
-
-  Ways to finalise
-    -> Closed -> Finalise
-    -> Illegal
-
-
-
--}
-
+  (isFinal (close state >>= finalise) & counterexample "close and finalise: ")
+    .||. (isFinal (finalise state) & counterexample "finalise directly: ")
 
 main :: IO ()
 main = do
   hspec $ do
     it "can't go from init to finalise" $ do
-      finalise init `shouldBe` Nothing
+      finalise (init emptyState) `shouldBe` Nothing
     it "can go to final from closed" $ do
-      (close init >>= finalise) `shouldBe` Just Final
+      isFinal (close (init emptyState) >>= finalise)
 
     describe "properties" $ do
       it "Can always close and finalise any state"
